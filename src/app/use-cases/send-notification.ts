8eacfe5f5b1f01d2dd93af282fc83f860abf8eb4
Vinicius Braz <vinicius_lsb@live.com>
2022-12-15 18:13:31 -0300

    chore: organize packages and imports

--- src/app/use-cases/send-notification.ts
+++ src/app/use-cases/send-notification.ts
@@ -1,6 +1,7 @@
 import { Notification } from './../entities/notification';
 import { Content } from '../entities/notification-content';
 import { NotificationRepository } from '../repositories/notification-repository';
+import { Injectable } from '@nestjs/common';
 
 interface SendNotificationRequest {
   recipientId: string;
@@ -12,6 +13,7 @@ interface SendNotificationResponse {
   notification: Notification;
 }
 
+@Injectable()
 export class SendNotification {
   constructor(private notificationRepository: NotificationRepository) {}
 
---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,11 @@
+import { SendNotification } from './../../app/use-cases/send-notification';
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { NotificationController } from './controllers/notification.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationController],
+  providers: [SendNotification],
+})
+export class HttpModule {}

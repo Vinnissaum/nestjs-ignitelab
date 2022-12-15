4fad2746d2bc83433db95147489204e2049f598c
Vinicius Braz <vinicius_lsb@live.com>
2022-12-15 15:31:45 -0300

    feat: add tests for notification

---
+++ src/app/use-cases/send-notification.spec.ts
@@ -0,0 +1,19 @@
+import { InMemoryNotificationRepository } from './../../../test/repositories/in-memory-notification-repository';
+import { SendNotification } from './send-notification';
+
+const inMemoryRepository = new InMemoryNotificationRepository();
+
+describe('Send notification', () => {
+  it('should be able to send a notification', async () => {
+    const sendNotification = new SendNotification(inMemoryRepository);
+
+    const { notification } = await sendNotification.execute({
+      category: 'test',
+      recipientId: '123',
+      content: 'notification',
+    });
+
+    expect(inMemoryRepository.notifications).toHaveLength(1);
+    expect(inMemoryRepository.notifications[0]).toEqual(notification);
+  });
+});

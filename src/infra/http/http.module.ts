ba0e39644cf843d06503cce363e9558e175913df
Vinicius Braz <vinicius_lsb@live.com>
2022-12-18 17:15:12 -0300

    chore: add providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
-import { SendNotification } from './../../app/use-cases/send-notification';
+import { SendNotification } from '@app/use-cases/send-notification';
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
-import { NotificationController } from './controllers/notification.controller';
+import { DatabaseModule } from '@infra/database/database.module';
+import { NotificationController } from '@infra/http/controllers/notification.controller';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@app/use-cases/count-recipient-notifications';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
+import { GetRecipientNotifications } from '@app/use-cases/get-recipient-notifications';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+    GetRecipientNotifications,
+  ],
 })
 export class HttpModule {}
